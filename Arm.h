9428c7aac194cee691fb342f426bf929647b3b8f
notthatsimple <scooteeroo@gmail.com>
2014-03-09 18:20:59 -0500

    Update Arm.h

--- Arm.h
+++ Arm.h
@@ -1,21 +1,10 @@
 #include "WPILib.h"
-
-
-
 class Arm {
-
 	Jaguar *raiseArm;  //Motors to control motion of the arm
-
 	Joystick *xboxController;
-
 public:
-
 	Arm();
-
 	void RaiseArm();
-
 	void LowerArm();
-
 	void OperateArm();
-
 };
