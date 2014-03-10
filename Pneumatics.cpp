fdf548f811ed3acebd002f7ca8ef0df1a12ee866
petertorze <peter.torzewski@charter.net>
2014-03-10 18:12:47 -0500

    Update Pneumatics.cpp

--- Pneumatics.cpp
+++ Pneumatics.cpp
@@ -2,17 +2,17 @@
 #include "Pneumatics.h"
 
 void Pneumatics::FireShooter(){
-	shooterSolenoid1->Set(DoubleSolenoid::kForward);
-	//shooterSolenoid2->Set(DoubleSolenoid.kForward);
-	//shooterSolenoid3->Set(DoubleSolenoid::kForward);
-	SmartDashboard::PutString("Button 6", "PRESSED ");
+	shooterSolenoid1->Set(DoubleSolenoid::kReverse);
+	shooterSolenoid2->Set(DoubleSolenoid::kReverse);
+	shooterSolenoid3->Set(DoubleSolenoid::kReverse);
 }
 
 void Pneumatics::RetractCylinder(){
-	shooterSolenoid1->Set(DoubleSolenoid::kReverse);
-	//shooterSolenoid2->Set(DoubleSolenoid::kOff);
-	//shooterSolenoid3->Set(DoubleSolenoid::kOff);
-	SmartDashboard::PutString("Button 6", "NOT PRESSED");
+	shooterSolenoid1->Set(DoubleSolenoid::kForward);
+	shooterSolenoid2->Set(DoubleSolenoid::kForward);
+	shooterSolenoid3->Set(DoubleSolenoid::kForward);
+	
+
 }
 void Pneumatics::Shooter(){
 
@@ -20,11 +20,21 @@ void Pneumatics::Shooter(){
 	if (joystick->GetRawButton(6)){
 		FireShooter();
 	}
+
+	else if (joystick->GetRawButton(5)){
+		FireMiddleCylinder();
+	}
 	else{
 		RetractCylinder();
-		}
+	}
 }
+void Pneumatics::FireMiddleCylinder(){
+		shooterSolenoid1->Set(DoubleSolenoid::kForward);
+	}
+
 Pneumatics::Pneumatics (){
-	shooterSolenoid1 = new DoubleSolenoid (2, 1);
-	joystick = new Joystick(1);
+	shooterSolenoid1 = new DoubleSolenoid (1, 2);
+	shooterSolenoid2 = new DoubleSolenoid (3, 4);
+	shooterSolenoid3 = new DoubleSolenoid (5, 6);
+	joystick = new Joystick(2);
 }
