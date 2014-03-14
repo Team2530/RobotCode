7d8f7f48b29d09a9a8c181c29cd6d542588814be
notthatsimple <scooteeroo@gmail.com>
2014-03-14 18:37:58 -0500

    Update Arm.cpp

--- Arm.cpp
+++ Arm.cpp
@@ -4,6 +4,7 @@ Arm::Arm() {
 	raiseArm = new Jaguar(9);  //Creat jag arm motor
 	xboxController = new Joystick(2);
 	joystick = new Joystick(1);
+	bool auto = false;
 }
 void Arm::RaiseArm() {
 		if ((xboxController->GetRawAxis(5) >= 0.5) || (joystick->GetRawAxis(6) >= 0.5))
@@ -18,23 +19,26 @@ void Arm::LowerArm() {
 			raiseArm->SetSpeed(0);
 }
 //For automatically lowering and raising the arm all the way to the top/bottom on the press of a button
-#if 0
+
 void Arm::AutoArm() {
-	/*if (xboxController->GetRawButton(3))
+	
+	if (xboxController->GetRawButton(3))
 		raiseArm->SetSpeed(1);
+		auto = true;
 	if (xboxController->GetRawButton(4))
 		raiseArm->SetSpeed(-1);
-		*/
+		auto = true;
+	else auto = false;
 }
 }
-#endif
 
 void Arm::OperateArm() {
+	AutoArm();
 	if ((xboxController->GetRawAxis(5) >= 0.5) || (joystick->GetRawAxis(6) >= 0.5))
 		RaiseArm();
 	else if ((xboxController->GetRawAxis(5) <= -0.5) || (joystick->GetRawAxis(6) <= -0.5))
 		LowerArm();
-	else
+	else if (!auto)
 		raiseArm->SetSpeed(0);
 }
 
