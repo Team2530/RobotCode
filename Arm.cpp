263178ec3cd9d0f786a46d6b3d490a32f5589b0a
notthatsimple <scooteeroo@gmail.com>
2014-03-13 21:32:34 -0500

    Update Arm.cpp
    Attempted to add an "automatic" mode for the arm, does not work yet.
    Removed software limit switches; it was determined that hardware limit switches do the job.

--- Arm.cpp
+++ Arm.cpp
@@ -1,33 +1,39 @@
 #include "Arm.h"
 #include "WPILib.h"
 Arm::Arm() {
-
-	top_limit_switch = new DigitalInput(1);
-	bottom_limit_switch = new DigitalInput(2);
-	
 	raiseArm = new Jaguar(9);  //Creat jag arm motor
 	xboxController = new Joystick(2);
 	joystick = new Joystick(1);
 }
 void Arm::RaiseArm() {
-
-	/*if (top_limit_switch->Get())
-		raiseArm->SetSpeed(0);
-	else*/
-		raiseArm->SetSpeed(1);
+		if ((xboxController->GetRawAxis(5) >= 0.5) || (joystick->GetRawAxis(6) >= 0.5))
+			raiseArm->SetSpeed(1);
+		else 
+			raiseArm->SetSpeed(0);
 }
 void Arm::LowerArm() {
-	
-	/*if (bottom_limit_switch->Get())
-		raiseArm->SetSpeed(0);
-	else*/
-		raiseArm->SetSpeed(-1);
+		if ((xboxController->GetRawAxis(5) <= -0.5) || (joystick->GetRawAxis(6) <= -0.5))
+			raiseArm->SetSpeed(-1);
+		else
+			raiseArm->SetSpeed(0);
 }
+//For automatically lowering and raising the arm all the way to the top/bottom on the press of a button
+#if 0
+void Arm::AutoArm() {
+	/*if (xboxController->GetRawButton(3))
+		raiseArm->SetSpeed(1);
+	if (xboxController->GetRawButton(4))
+		raiseArm->SetSpeed(-1);
+		*/
+}
+}
+#endif
+
 void Arm::OperateArm() {
 	if ((xboxController->GetRawAxis(5) >= 0.5) || (joystick->GetRawAxis(6) >= 0.5))
-		raiseArm->SetSpeed(1);
+		RaiseArm();
 	else if ((xboxController->GetRawAxis(5) <= -0.5) || (joystick->GetRawAxis(6) <= -0.5))
-		raiseArm->SetSpeed(-1);
+		LowerArm();
 	else
 		raiseArm->SetSpeed(0);
 }
