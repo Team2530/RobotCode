6ea2fa9c10909b16d410fa7c6ad705ee3b408fc2
notthatsimple <scooteeroo@gmail.com>
2014-03-13 21:33:00 -0500

    Update Arm.h

--- Arm.h
+++ Arm.h
@@ -3,8 +3,6 @@ class Arm {
 	Jaguar *raiseArm;  //Motors to control motion of the arm
 	Joystick *xboxController;
 	Joystick *joystick;
-	DigitalInput *top_limit_switch;
-	DigitalInput *bottom_limit_switch;
 public:
 	Arm();
 	void RaiseArm();
